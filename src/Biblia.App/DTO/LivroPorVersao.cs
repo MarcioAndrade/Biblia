abce56ec9d067b076c08dd4d81ddf6922678e734
Marcio Costa <marcio281@gmail.com>
2019-11-18 22:16:34 -0300

    Refatoracao

--- src/Biblia.App/DTO/LivroPorVersao.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Biblia.App.DTO
-{
-    public class LivroPorVersao
-    {
-        public int Id { get; set; }
-        public Testamento Testamento { get; set; }
-        public Livro Livro { get; set; }
-    }
-}
--- src/Biblia.App/DTO/Testamento.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Biblia.App.DTO
-{
-    public class Testamento
-    {
-        public int Id { get; set; }
-        public string Descricao { get; set; }
-    }
-}

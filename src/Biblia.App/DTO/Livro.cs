c1afd90950f9e030028c324b290df3428f792ba2
Marcio Costa <marcio281@gmail.com>
2019-11-20 22:12:47 -0300

    Adicao de classes DTO para mapper

---
+++ src/Biblia.App/DTO/Livro.cs
@@ -0,0 +1,10 @@
+﻿namespace Biblia.App.DTO
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int TestamentoId { get; set; }
+        public int Posicao { get; set; }
+    }
+}

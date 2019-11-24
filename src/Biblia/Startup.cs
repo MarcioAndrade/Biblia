73d8eb20eddc486627f5c14c6655b17fdcd42637
Marcio Costa <marcio281@gmail.com>
2019-11-24 11:33:24 -0300

    Refatoração

--- src/Biblia/Startup.cs
+++ src/Biblia/Startup.cs
@@ -24,7 +24,7 @@ namespace Biblia
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<VersiculoRepository>();
+            services.AddTransient<BibliaRepository>();
             services.AddScoped<IBibliaApp, BibliaApp>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);

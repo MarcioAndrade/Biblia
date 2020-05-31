b1dd8d7305db1fe964108ed32bcf1ba4603e0e44
Marcio Costa <marcio281@gmail.com>
2020-05-30 23:24:20 -0300

    Ajustes na formatação da página de caixinha de promessas

--- client/biblia-app/src/app/app.component.ts
+++ client/biblia-app/src/app/app.component.ts
@@ -17,7 +17,9 @@ export class AppComponent {
   title = 'biblia-app';
 
   constructor(private _service: BibliaService) {
-
+    this._service.getCaixinhaPromessa()
+      .subscribe((data: CaixaPromessa) => this.caixaPromessa = data,
+        error => console.log(error));
   }
 
   ObterCaixinhaPromessa(): void {
